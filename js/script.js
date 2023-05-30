2c5192b6768cc90a75f127ac3c344942ef730417
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-05-30 17:43:19 +0200

    print in page

--- js/script.js
+++ js/script.js
@@ -2,29 +2,29 @@
 console.log("ok js")
 
 // recupero elemento dal DOM
+const blackboard = document.getElementById('blackboard');
 
-const blackboard = document.getElementById("blacboard");
-//check
-console.log(blackboard);
 
 // creo un ciclo che ripeta un numero da 1 a 100
-
 for(let i = 1; i <= 100; i++) {
     //creo variabile frizz
     let word = '';
     //controllo i multipli di i
-    if(!(i % 3)){
-       word = 'Fizz';
-    } else if (!(i % 5)){
-        word = 'Buzz'
-    }
-    
     if(!(i % 3) && !(i % 5)){
         word = 'FizzBuzz'
-    }else if (!(word)){
+    }else if(!(i % 3)){
+        word = 'Fizz';
+    } else if (!(i % 5)){
+        word = 'Buzz'
+    }else{
         word = i
     }
 
     console.log(word);
+
+    blackboard.innerText += '  ' + word;
 }
+
+
+
 
