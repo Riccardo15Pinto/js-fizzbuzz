3e83656f92eca9fe1aad46ac5d88619c3130d843
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-05-30 17:46:29 +0200

    fix space

--- js/script.js
+++ js/script.js
@@ -4,7 +4,6 @@ console.log("ok js")
 // recupero elemento dal DOM
 const blackboard = document.getElementById('blackboard');
 
-
 // creo un ciclo che ripeta un numero da 1 a 100
 for(let i = 1; i <= 100; i++) {
     //creo variabile frizz
@@ -22,7 +21,7 @@ for(let i = 1; i <= 100; i++) {
 
     console.log(word);
 
-    blackboard.innerText += '  ' + word;
+    blackboard.innerText += ' ' + word;
 }
 
 
